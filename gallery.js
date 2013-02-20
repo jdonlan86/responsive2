232aa7f002764746816c2c9d969f881dc72b994a
James Donlan <jdonlan2@gmail.com>
2013-02-19 20:13:04 -0700

    all the cw folder files

---
+++ gallery.js
@@ -0,0 +1,13 @@
+$('#gallery-brewers').cycle();
+
+enquire.register("screen and (min-width:1024px)",{
+match: function(){
+	$('#gallery-brewers').cycle();
+},
+unmatch: function(){
+	$('#gallery-brewers').cycle('destroy');
+	$('#gallery-brewers img').attr('style','');
+
+}
+
+}).listen();
